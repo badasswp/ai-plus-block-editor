a796596aec0a75f43d5e082e2f1f630dcc05a8fb
badasswp <badasswpdev@gmail.com>
2024-10-28 10:27:09 +0100

    chore: establish plugin entry file

---
+++ ai-plus-block-editor.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Plugin Name: AI + Block Editor
+ * Plugin URI:  https://github.com/badasswp/ai-plus-block-editor
+ * Description: Add AI Capabilities to the WP Block Editor.
+ * Version:     1.0.0
+ * Author:      badasswp
+ * Author URI:  https://github.com/badasswp
+ * License:     GPL v2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-3.0.html
+ * Text Domain: ai-plus-block-editor
+ * Domain Path: /languages
+ *
+ * @package AiPlusBlockEditor
+ */
+
+namespace badasswp\AiPlusBlockEditor;
+
+if ( ! defined( 'WPINC' ) ) {
+	exit;
+}
+
+define( 'APBE_AUTOLOAD', __DIR__ . '/vendor/autoload.php' );
+
+// Composer Check.
+if ( ! file_exists( APBE_AUTOLOAD ) ) {
+	add_action(
+		'admin_notices',
+		function () {
+			vprintf(
+				/* translators: Plugin directory path. */
+				esc_html__( 'Fatal Error: Composer not setup in %s', 'ai-plus-block-editor' ),
+				[ __DIR__ ]
+			);
+		}
+	);
+
+	return;
+}
+
+// Run Plugin.
+require_once APBE_AUTOLOAD;
+( \AiPlusBlockEditor\Plugin::get_instance() )->run();
