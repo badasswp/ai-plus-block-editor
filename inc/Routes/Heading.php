5966e021eb06266b306a0d1a5bfd4f19f2c7f1a0
badasswp <badasswpdev@gmail.com>
2024-10-30 19:03:00 +0100

    fix: typo in prompt

--- inc/Routes/Heading.php
+++ inc/Routes/Heading.php
@@ -85,7 +85,7 @@ class Heading extends Route implements Router {
 		return rest_ensure_response(
 			$ai->get_ai_response(
 				[
-					'content' => "Using the following text, generate a causal tone: $article",
+					'content' => "Using the following text, generate a casual tone: $article",
 				]
 			)
 		);
