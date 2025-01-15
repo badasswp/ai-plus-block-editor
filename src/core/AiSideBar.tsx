0e1e8f27c377fbbe87f79fa89fdf06d77e2f958b
badasswp <badasswpdev@gmail.com>
2025-01-15 19:40:37 +0100

    feat: implement Sidebar feature

---
+++ src/core/AiSideBar.tsx
@@ -0,0 +1,54 @@
+import { __ } from '@wordpress/i18n';
+import { verse } from '@wordpress/icons';
+import { Fragment } from '@wordpress/element';
+import { PanelBody } from '@wordpress/components';
+import { registerPlugin } from '@wordpress/plugins';
+import { PluginSidebar, PluginSidebarMoreMenuItem } from '@wordpress/edit-post';
+
+import Slug from '../components/Slug';
+import Headline from '../components/Headline';
+
+import '../styles/app.scss';
+
+/**
+ * AI SideBar.
+ *
+ * This function returns a JSX component that comprises
+ * the Plugin's Sidebar and controls.
+ *
+ * @since 1.0.0
+ *
+ * @returns {JSX.Element}
+ */
+const AiSideBar = (): JSX.Element => {
+  return (
+    <Fragment>
+      <PluginSidebarMoreMenuItem
+        icon={ verse }
+        target="apbe-sidebar"
+      >
+        { __( 'AI + Block Editor', 'ai-plus-block-editor' ) }
+      </PluginSidebarMoreMenuItem>
+      <PluginSidebar
+        name="apbe-sidebar"
+        title={ __( 'AI + Block Editor', 'ai-plus-block-editor' ) }
+        icon={ verse }
+      >
+        <PanelBody>
+          <div className="apbe-sidebar">
+            <ul>
+              <li>
+                <Headline />
+              </li>
+              <li>
+                <Slug />
+              </li>
+            </ul>
+          </div>
+        </PanelBody>
+      </PluginSidebar>
+    </Fragment>
+  );
+};
+
+registerPlugin( 'ai-plus-block-editor', { render: AiSideBar } );
