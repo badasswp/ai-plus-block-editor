a1738df4fd9c0c7065a316ee35e2848683b4b5ef
badasswp <badasswpdev@gmail.com>
2025-01-26 17:18:21 +0100

    test: add tests for SideBar route

--- tests/php/Routes/SideBarTest.php
+++ tests/php/Routes/SideBarTest.php
@@ -8,7 +8,7 @@ use AiPlusBlockEditor\Routes\SideBar;
 use AiPlusBlockEditor\Abstracts\Service;
 
 /**
- * @covers \AiPlusBlockEditor\Routes\SideBar::is_sql
+ * @covers \AiPlusBlockEditor\Routes\SideBar::response
  */
 class SideBarTest extends TestCase {
 	public SideBar $sidebar;
@@ -26,5 +26,48 @@ class SideBarTest extends TestCase {
 	public function test_route_initial_values() {
 		$this->assertSame( $this->sidebar->method, 'POST' );
 		$this->assertSame( $this->sidebar->endpoint, '/sidebar' );
+	}
+
+	public function test_response_bails_out_if_no_prompt_text() {
+		$sidebar = Mockery::mock( SideBar::class )->makePartial();
+		$sidebar->shouldAllowMockingProtectedMethods();
+
+		$request = Mockery::mock( \WP_REST_Request::class )->makePartial();
+		$request->shouldAllowMockingProtectedMethods();
+
+		$request->shouldReceive( 'get_json_params' )
+			->andReturn(
+				[
+					'text' => '',
+				]
+			);
+
+		$sidebar->request = $request;
+
+		$this->assertInstanceOf( \WP_Error::class, $sidebar->response() );
+		$this->assertConditionsMet();
+	}
+
+	public function test_response_passes() {
+		$sidebar = Mockery::mock( SideBar::class )->makePartial();
+		$sidebar->shouldAllowMockingProtectedMethods();
+
+		$request = Mockery::mock( \WP_REST_Request::class )->makePartial();
+		$request->shouldAllowMockingProtectedMethods();
+
+		$request->shouldReceive( 'get_json_params' )
+			->andReturn(
+				[
+					'text' => 'Generate an SEO friendly Headline for: Hello World!',
+				]
+			);
+
+		$sidebar->shouldReceive( 'get_response' )
+			->andReturn( 'What a Wonderful World!' );
+
+		$sidebar->request = $request;
+
+		$this->assertInstanceOf( \WP_REST_Response::class, $sidebar->response() );
+		$this->assertConditionsMet();
 	}
 }
