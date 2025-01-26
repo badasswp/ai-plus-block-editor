d2ca053517e73d7e33cc506e9611c8ad14b3fb39
badasswp <badasswpdev@gmail.com>
2025-01-26 16:36:08 +0100

    test: add tests for run method

--- tests/php/Providers/OpenAITest.php
+++ tests/php/Providers/OpenAITest.php
@@ -8,7 +8,7 @@ use AiPlusBlockEditor\Providers\OpenAI;
 use Orhanerday\OpenAi\OpenAi as ChatGPT;
 
 /**
- * @covers \AiPlusBlockEditor\Providers\OpenAI::__construct
+ * @covers \AiPlusBlockEditor\Providers\OpenAI::run
  * @covers \AiPlusBlockEditor\Providers\OpenAI::get_default_args
  */
 class OpenAITest extends TestCase {
@@ -17,29 +17,11 @@ class OpenAITest extends TestCase {
 	public function setUp(): void {
 		\WP_Mock::setUp();
 
-		\WP_Mock::userFunction( 'get_option' )
-			->with( 'ai_plus_block_editor', [] )
-			->andReturn(
-				[
-					'open_ai_token' => 'age38gegewjdhagepkhif',
-				]
-			);
-
 		$this->open_ai = new OpenAI();
 	}
 
 	public function tearDown(): void {
 		\WP_Mock::tearDown();
-	}
-
-	public function test_chat_gpt_instance() {
-		$reflection = new \ReflectionClass( $this->open_ai );
-		$property   = $reflection->getProperty( 'ai' );
-
-		$property->setAccessible( true );
-		$ai_instance = $property->getValue( $this->open_ai );
-
-		$this->assertInstanceOf( ChatGPT::class, $ai_instance );
 	}
 
 	public function test_get_default_args() {
@@ -77,5 +59,51 @@ class OpenAITest extends TestCase {
 				'presence_penalty'  => 0,
 			]
 		);
+	}
+
+	public function test_run() {
+		$open_ai = Mockery::mock( OpenAI::class )->makePartial();
+		$open_ai->shouldAllowMockingProtectedMethods();
+
+		$chat_gpt = Mockery::mock( ChatGPT::class )->makePartial();
+		$chat_gpt->shouldAllowMockingProtectedMethods();
+
+		$open_ai->shouldReceive( 'get_default_args' )
+			->andReturn(
+				[
+					'model'             => 'gpt-3.5-turbo',
+					'temperature'       => 1.0,
+					'max_tokens'        => 4000,
+					'frequency_penalty' => 0,
+					'presence_penalty'  => 0,
+				]
+			);
+
+		\WP_Mock::userFunction( 'wp_parse_args' )
+			->andReturnUsing(
+				function ( $arg1, $arg2 ) {
+					return array_merge( $arg2, $arg1 );
+				}
+			);
+
+		\WP_Mock::userFunction( 'get_option' )
+			->with( 'ai_plus_block_editor', [] )
+			->andReturn(
+				[
+					'open_ai_token' => 'age38gegewjdhagepkhif',
+				]
+			);
+
+		$chat_gpt->shouldReceive( 'chat' )
+			->andReturn( '{"choices":[{"message":{"content":"What a Wonderful World!"}}]}' );
+
+		$response = $open_ai->run(
+			[
+				'content' => 'Generate me an SEO friendly Headline using: Hello World!',
+			]
+		);
+
+		$this->assertSame( $response, '' );
+		$this->assertConditionsMet();
 	}
 }
