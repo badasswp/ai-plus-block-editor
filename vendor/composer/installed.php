6c096e49899d0cd6ccfe75249f00be23e9c75801
badasswp <badasswpdev@gmail.com>
2025-02-05 21:43:24 +0000

    push to master-built

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'badasswp/ai-plus-block-editor',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'c2447b986376ec68a64de8c820a76879357cd128',
+        'type' => 'package',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'badasswp/ai-plus-block-editor' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'c2447b986376ec68a64de8c820a76879357cd128',
+            'type' => 'package',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'orhanerday/open-ai' => array(
+            'pretty_version' => '5.2',
+            'version' => '5.2.0.0',
+            'reference' => 'd8c78fe2f5fed59e0ba458f90b5589ed9f13a367',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../orhanerday/open-ai',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
