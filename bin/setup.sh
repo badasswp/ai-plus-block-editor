b8906d3e3348ffcd42188663d0f48fb5994208f6
badasswp <badasswpdev@gmail.com>
2025-04-06 23:39:35 +0100

    chore: update WP bash scripts

--- bin/setup.sh
+++ bin/setup.sh
@@ -2,3 +2,5 @@
 
 wp-env run cli wp theme activate twentytwentythree
 wp-env run cli wp rewrite structure /%postname%
+wp-env run cli wp option update blogname "AI + Block Editor"
+wp-env run cli wp option update blogdescription "Add AI Capabilities to the WP Block Editor."
