db376d764ea06bf54a2ddcc79576b15607afab0d
badasswp <badasswpdev@gmail.com>
2025-04-17 06:02:31 +0100

    feat: add store and types

---
+++ src/utils/store.ts
@@ -0,0 +1,2 @@
+export const editorStore = 'core/editor';
+export const blockEditorStore = 'core/block-editor';
---
+++ src/utils/types.ts
@@ -0,0 +1,12 @@
+export interface selectProps {
+	getCurrentPostId: () => number;
+	getEditedPostContent: () => any;
+	getEditedPostAttribute: ( attribute: string ) => any;
+}
+
+export interface selectBlockProps {
+	getSelectedBlock: () => any;
+	getSelectedBlockClientId: () => string;
+	getBlockAttributes: ( clientId: string ) => any;
+	getBlock: ( clientId: string ) => any;
+}
