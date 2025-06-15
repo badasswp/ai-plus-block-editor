38dfdd521ecbbb2a7df261a7d29ad4ce3765b4c5
badasswp <badasswpdev@gmail.com>
2025-06-15 17:38:09 +0100

    chore: export all components

---
+++ src/components/index.tsx
@@ -0,0 +1,7 @@
+import SEO from '../components/SEO';
+import Slug from '../components/Slug';
+import Summary from '../components/Summary';
+import Headline from '../components/Headline';
+import Switcher from '../components/Switcher';
+
+export { SEO, Slug, Summary, Headline, Switcher };
