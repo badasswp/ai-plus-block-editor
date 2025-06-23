96e38c6fe0d63a08ccf99ab186eeec3fe19da8b2
badasswp <badasswpdev@gmail.com>
2025-06-23 02:52:35 +0100

    test: update test cover tags

--- tests/php/Providers/GeminiTest.php
+++ tests/php/Providers/GeminiTest.php
@@ -8,6 +8,7 @@ use AiPlusBlockEditor\Providers\Gemini;
 
 /**
  * @covers \AiPlusBlockEditor\Providers\Gemini::run
+ * @covers \AiPlusBlockEditor\Providers\Gemini::get_api_url
  * @covers \AiPlusBlockEditor\Providers\Gemini::get_default_args
  * @covers \AiPlusBlockEditor\Providers\Gemini::get_json_error
  * @covers \AiPlusBlockEditor\Admin\Options::__callStatic
