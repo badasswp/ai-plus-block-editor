e41c884042dc673bc47927f4b862499fade79cff
badasswp <badasswpdev@gmail.com>
2025-09-12 14:07:26 +0100

    feat: add Grok options

--- inc/Admin/Options.php
+++ inc/Admin/Options.php
@@ -135,6 +135,7 @@ class Options {
 							'OpenAI'   => 'ChatGPT',
 							'Gemini'   => 'Gemini',
 							'DeepSeek' => 'DeepSeek',
+							'Grok'     => 'Grok',
 						],
 					],
 				],
@@ -180,6 +181,22 @@ class Options {
 						'summary' => esc_html__( 'Use DeepSeek capabilities in Block Editor', 'ai-plus-block-editor' ),
 					],
 					'deepseek_token'  => [
+						'control'     => esc_attr( 'password' ),
+						'placeholder' => esc_attr( '' ),
+						'label'       => esc_html__( 'API Keys', 'ai-plus-block-editor' ),
+						'summary'     => esc_html__( 'e.g. ae2kgch7ib9eqcbeveq9a923nv87392av', 'ai-plus-block-editor' ),
+					],
+				],
+			],
+			'grok_options'          => [
+				'heading'  => esc_html__( 'Grok', 'ai-plus-block-editor' ),
+				'controls' => [
+					'grok_enable' => [
+						'control' => esc_attr( 'checkbox' ),
+						'label'   => esc_html__( 'Enable Grok', 'ai-plus-block-editor' ),
+						'summary' => esc_html__( 'Use Grok capabilities in Block Editor', 'ai-plus-block-editor' ),
+					],
+					'grok_token'  => [
 						'control'     => esc_attr( 'password' ),
 						'placeholder' => esc_attr( '' ),
 						'label'       => esc_html__( 'API Keys', 'ai-plus-block-editor' ),
