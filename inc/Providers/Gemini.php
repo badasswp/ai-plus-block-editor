e22e938028f755c1172daeaee77a7a6f13009709
activist507 <96887053+activist507@users.noreply.github.com>
2026-03-03 12:11:44 +0100

    Update inc/Providers/Gemini.php
    Co-authored-by: badasswp <badasswpdev@gmail.com>

--- inc/Providers/Gemini.php
+++ inc/Providers/Gemini.php
@@ -89,7 +89,7 @@ class Gemini extends Provider implements ProviderInterface {
 	 * @since 1.5.0
 	 *
 	 * @param mixed[] $payload JSON Payload.
-	 * @return string|WP_Error
+	 * @return string|\WP_Error
 	 */
 	public function run( $payload ) {
 		$api_key = get_option( Options::get_page_option(), [] )['google_gemini_token'] ?? '';
