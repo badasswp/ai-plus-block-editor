07ee0f6c160e6280b89b44d3a467009d8dd32ee1
activist507 <96887053+activist507@users.noreply.github.com>
2026-03-03 12:10:51 +0100

    Update inc/Providers/DeepSeek.php
    Co-authored-by: badasswp <badasswpdev@gmail.com>

--- inc/Providers/DeepSeek.php
+++ inc/Providers/DeepSeek.php
@@ -86,7 +86,7 @@ class DeepSeek extends Provider implements ProviderInterface {
 	 * @since 1.6.0
 	 *
 	 * @param mixed[] $payload JSON Payload.
-	 * @return string|WP_Error
+	 * @return string|\WP_Error
 	 */
 	public function run( $payload ) {
 		$api_key = get_option( Options::get_page_option(), [] )['deepseek_token'] ?? '';
