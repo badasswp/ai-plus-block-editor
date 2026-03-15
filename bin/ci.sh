bc9af6b2ce795194032366a67bfab22c274acff0
badasswp <badasswpdev@gmail.com>
2026-03-15 10:08:13 +0100

    chore: update CI/CD steps

--- bin/ci.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-yarn wp-env run cli wp theme activate twentytwentythree
-yarn wp-env run cli wp rewrite structure /%postname%
-yarn wp-env run cli wp option update blogname "AI + Block Editor"
-yarn wp-env run cli wp option update blogdescription "Add AI Capabilities to the WP Block Editor."
