bc9dc5a6b3a5789781a132e7b0a95dbc45661513
badasswp <badasswpdev@gmail.com>
2026-03-23 07:15:36 +0000

    push to master-built

---
+++ dist/app.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-notices', 'wp-plugins', 'wp-primitives'), 'version' => 'ae71e36bb536c261d05a');
