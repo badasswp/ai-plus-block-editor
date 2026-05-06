c04b0c451cb34fa511836067028469a852814a05
badasswp <badasswpdev@gmail.com>
2026-05-06 19:22:45 +0000

    Build from release-1.9.1

--- src/utils/store.ts
+++
@@ -1,3 +0,0 @@
-export const editorStore = 'core/editor';
-export const noticesStore = 'core/notices';
-export const blockEditorStore = 'core/block-editor';
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'badasswp/ai-plus-block-editor',
+        'pretty_version' => 'dev-release-1.9.1-built',
+        'version' => 'dev-release-1.9.1-built',
+        'reference' => '366917817f6c6f85e582fbc98ee5f805655e58df',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'badasswp/ai-plus-block-editor' => array(
+            'pretty_version' => 'dev-release-1.9.1-built',
+            'version' => 'dev-release-1.9.1-built',
+            'reference' => '366917817f6c6f85e582fbc98ee5f805655e58df',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'orhanerday/open-ai' => array(
+            'pretty_version' => '5.3',
+            'version' => '5.3.0.0',
+            'reference' => '4ba9c704197b91260c89a1dc147cf46f81824f02',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../orhanerday/open-ai',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
