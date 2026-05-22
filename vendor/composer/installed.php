1b18d9ebfe33312aa924ae8fb840519950ba39c3
badasswp <badasswpdev@gmail.com>
2026-05-22 16:41:09 +0000

    Build from release-1.9.2

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'badasswp/ai-plus-block-editor',
+        'pretty_version' => 'dev-release-1.9.2-built',
+        'version' => 'dev-release-1.9.2-built',
+        'reference' => '545e52dda33c6640c43d567d4c23e2472261c09b',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'badasswp/ai-plus-block-editor' => array(
+            'pretty_version' => 'dev-release-1.9.2-built',
+            'version' => 'dev-release-1.9.2-built',
+            'reference' => '545e52dda33c6640c43d567d4c23e2472261c09b',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'orhanerday/open-ai' => array(
+            'pretty_version' => '5.3',
+            'version' => '5.3.0.0',
+            'reference' => '4ba9c704197b91260c89a1dc147cf46f81824f02',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../orhanerday/open-ai',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
