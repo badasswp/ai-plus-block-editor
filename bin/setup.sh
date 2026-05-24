157431a0132b4a0d473260cba2578005edea45af
badasswp <badasswpdev@gmail.com>
2026-05-24 13:31:43 +0000

    push to master-built

--- bin/setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-wp-env run cli wp theme activate twentytwentythree
-wp-env run cli wp rewrite structure /%postname%
-wp-env run cli wp option update blogname "AI + Block Editor"
-wp-env run cli wp option update blogdescription "Add AI Capabilities to the WP Block Editor."
