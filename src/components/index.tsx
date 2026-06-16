39e03f42806bfec8dff915027f684906774a5064
badasswp <badasswpdev@gmail.com>
2026-06-16 08:49:07 +0000

    push to master-built

--- src/components/index.tsx
+++
@@ -1,8 +0,0 @@
-import SEO from '../components/SEO';
-import Slug from '../components/Slug';
-import Summary from '../components/Summary';
-import Headline from '../components/Headline';
-import Switcher from '../components/Switcher';
-import Social from '../components/Social';
-
-export { SEO, Slug, Summary, Headline, Switcher, Social };
---
+++ vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'name' => 'badasswp/ai-plus-block-editor',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '987ff76cabae7472e34ce82a05df731b61320a21',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'badasswp/ai-plus-block-editor' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '987ff76cabae7472e34ce82a05df731b61320a21',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'badasswp/pluginate' => array(
+            'pretty_version' => 'v1.0.0',
+            'version' => '1.0.0.0',
+            'reference' => '2b28213dcc47ce0dc524e904a4bc8f51341fd5b5',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../badasswp/pluginate',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'orhanerday/open-ai' => array(
+            'pretty_version' => '5.3',
+            'version' => '5.3.0.0',
+            'reference' => '4ba9c704197b91260c89a1dc147cf46f81824f02',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../orhanerday/open-ai',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
